e6787253f49d80ee0a9e9257b5d85502618808f6
51115152017.2 <51115152017.2@51DE194267.senacrjedu.local>
2018-03-01 21:46:19 -0300

    Primeiro Commit

---
+++ hello_word.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Minha primeira pagia em PHP com basico "Olá Mundo"</h1>
+    <code>
+        <!--para iniciantes um bloco de código PHP usamos as tags abaixo-->
+        <?php
+            # para declarar uma variável é necessário usar o simples simbolo $
+            $hello = "Olá Mundo PHP";
+
+            // Comando utilizado para imprimir(saida) na tela
+            echo($hello);
+        
+            /*
+            Detalhes importantes:
+
+            Em PHP sempre será necessario iniciar uma variavel com $.
+            Para que o servidor entenda os códigos PHP os arquivos devem ser salvos com a extensão .php.
+
+            */
+        ?>  
+    </code>
+
+</body>
+</html>
